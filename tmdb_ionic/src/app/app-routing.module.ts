53c076223c8a67775ab67929a595431e75694216
joewill <joewill@ecs.vuw.ac.nz>
2019-07-18 12:46:23 +1200

    Initialise Firebase Login Page
    Generate login page file. Set route so that login is starting page.
    Add input fields for email and  password.
    Add buttons to login and to redirect to registration page.

--- tmdb_ionic/src/app/app-routing.module.ts
+++ tmdb_ionic/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" }
 ];
 
 @NgModule({
@@ -12,4 +12,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
